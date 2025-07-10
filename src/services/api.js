d5192cd4cc2d85b308168b1124e70a4d393a4d7d
Edmond <unstopableenergy@gmail.com>
2025-07-10 18:42:05 +0000

    backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://bookstore-api-40ll.onrender.com", // your backend base URL
+  baseURL: "https://bookstore-api-40ll.onrender.com", // backend base URL
 });
 
 // Attach JWT token to all requests if available
